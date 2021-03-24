07c0db373b223b5382689da439346f820b48ff37
Danielle_M <daniellematalonis@gmail.com>
2021-03-23 23:24:41 -0400

    Added a way to determine seasons and display winter with text and snowflakes. Summer currently just has text.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
@@ -7,22 +8,21 @@ constructor(props) {
     super(props);
 
     this.state = { lat: null, errorMessage: "" };
+}
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState ({ lat:position.coords.latitude });
-                },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState ({ lat:position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
 }
+
     render() {
        if (this.state.errorMessage && !this.state.lat) {
            return <div>Error: {this.state.errorMessage}</div>
        }
        if (!this.state.errorMessage && this.state.lat) {
-          return <div>Latitude: {this.state.lat}</div>
+          return <SeasonDisplay lat={this.state.lat} />
        }
        return <div>Loading...</div>;
     }
