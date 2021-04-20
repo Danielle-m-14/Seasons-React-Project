5c9bae6dae3b42fa970987a08c9161254fbbc428
Danielle_M <daniellematalonis@gmail.com>
2021-04-19 23:24:22 -0400

    Added spring and fall to getSeason

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -9,14 +9,29 @@ const seasonConfig = {
   winter: {
     text: "Burr, it's chilly!",
     iconName: "snowflake"
+  },
+  spring: {
+    text: '"Blossom by blossom the spring begins." -Algernon Charles Swinburne',
+    iconName: "flower"
+  },
+  fall: {
+    text: '"Autumn carries more gold in its pocket than all the other seasons." -Jim Bishop',
+    iconName: "leaves"
   }
 };
 
 const getSeason = (lat, month) => {
-  if (month > 2 && month < 9) {
-    return lat > 0 ? 'summer' : 'winter';
-  } else {  
-      return lat > 0 ? 'winter' : 'summer';
+   if (month > 1 && month < 5) {
+    return lat > 0 ? 'spring' : 'fall';
+   }
+   if (month > 4 && month < 8) {
+     return lat > 0 ? 'summer' : 'winter';
+   }
+   if (month > 7 && month < 11) {
+     return lat > 0 ? 'fall' : 'spring';
+   }
+   else {  
+     return lat > 0 ? 'winter' : 'summer';
   }
 };
 
